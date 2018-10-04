c02bd4da7272a56d2a2a0e03eab413cfa844ad5c
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-03 18:19:43 -0600

    Update seeds.rb with a class & methods to parse & format CSV data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,35 @@
 
+# How Should I be testing these -- where does my test folder go? -- I need to install Minitest -- need permission.
+
+
+require 'CSV'
+
+# require_relative './data/comedians.csv'
+
+class LaughData
+
+  def self.format_data(path)
+    arr = CSV.parse(path)
+    head, *tail = arr
+    create_hash(head, tail)
+  end
+
+  def self.create_hash(headers, rows)
+    return "Incorrect pairing" if headers.count != rows.first.count
+    array = rows.map { |row| make_row(row, headers) }
+  end
+
+  private
+
+  def self.make_row(row, headers)
+    hash = {}
+    row.each.with_index { |data, index|
+      hash[headers[index].to_sym] = data
+    }
+  end
+
+end
+
+
+comedians_path = './data/comedians.csv'
+comedians_list = LaughData.format_data
