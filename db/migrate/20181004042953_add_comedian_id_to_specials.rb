d352f1e01f20dbdbe3c8e7831020fd6bc5222539
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-03 22:30:59 -0600

    create a migration for updating specials with comedian_id

---
+++ db/migrate/20181004042953_add_comedian_id_to_specials.rb
@@ -0,0 +1,4 @@
+class AddComedianIdToSpecials < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
 
+
+comedian1 = {name: "Jerry Seinfeld", age: 64, hometown:Massapequa - New York}
