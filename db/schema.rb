84ad2f839a026c561472b1411b57b9ddf8820711
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-03 22:33:39 -0600

    create & migrate update complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_031428) do
+ActiveRecord::Schema.define(version: 2018_10_04_042953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_10_04_031428) do
     t.text "name"
     t.integer "runtime"
     t.text "image"
+    t.integer "comedian_id"
   end
 
 end
