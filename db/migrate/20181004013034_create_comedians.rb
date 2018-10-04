bf436a2b2b61a5c2ce2e2a7bf10fc10e3bf820dd
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-03 19:48:37 -0600

    create the schema via db:create & db:migrate

--- db/migrate/20181004013034_create_comedians.rb
+++ db/migrate/20181004013034_create_comedians.rb
@@ -1,4 +1,14 @@
+
 class CreateComedians < ActiveRecord::Migration[5.2]
+
   def change
+    create_table :comedians do |table|
+      table.text    :name
+      table.integer :age
+      table.text    :hometown
+
+      # table.timestamps null: true
+    end
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_04_013034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comedians", force: :cascade do |t|
+    t.text "name"
+    t.integer "age"
+    t.text "hometown"
+  end
 
 end
