0febdb5dde1fa0166379a0ca745352fa13b270e0
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-05 17:57:02 -0600

    In the controller - moved the 2nd ERB argument to a new line -- still works, better visually.

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -6,8 +6,8 @@ class LaughTracksApp < Sinatra::Base
   get '/comedians' do
     comedians = Comedian.assess_params(params)
     specials = comedians.find_specials
-    # binding.pry
-    erb :"comedians/index", locals: { comedians: comedians, specials: specials }
+    erb :"comedians/index",
+        locals: { comedians: comedians, specials: specials }
   end
 
 
