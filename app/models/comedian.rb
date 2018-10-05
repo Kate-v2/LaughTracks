3e6ef2670a6fb2a7f7922b28de2c08c515f93e8f
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-05 10:33:12 -0600

    Learned new ways to use active records -- see notes that will be deleted with next commit

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -12,19 +12,31 @@ class Comedian < ActiveRecord::Base
   end
 
   def self.average_age(params = {})
-    if params[:age]
-      params[:age].to_i
-    else
-      average(:age)
-    end
+    comics = assess_params(params)
+    comics.average(:age)
+    # if age = params[:age]
+    #   filtered = filter_by_age(age)
+    #   filtered.average(:age)
+    #   # @age_filtered.average(:age)
+    # else
+    #   average(:age)
+    # end
   end
 
   def self.cities(params = {})
-    pluck(:hometown).uniq
+    # pluck(:hometown).uniq
+    distinct.pluck(:hometown)
   end
 
+
+  # --- Filter ----
+
   def self.filter_by_age(age)
-    all.select { |comic| comic.age == age.to_i }
+    # all.select { |comic| comic.age == age.to_i }
+    # ^^^ This is ruby
+    # select(:age == age)  ?
+    # all.where(:age == age)
+    @age_filtered = where({age: age})
   end
 
 
