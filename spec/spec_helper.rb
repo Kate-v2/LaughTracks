1eb548b200a9d4711e99bc989fcd78a5ec4417a1
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-06 21:12:24 -0600

    update gitignore for simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
 ENV["RACK_ENV"] ||= "test"
 
-require 'bundler'
+require 'simplecov'
+SimpleCov.start
 
+
+require 'bundler'
 Bundler.require(:default, :test)
 require File.expand_path('../../config/environment.rb', __FILE__)
 require 'capybara/dsl'
