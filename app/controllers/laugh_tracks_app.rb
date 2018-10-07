441d5825b2158b59541ddbfb839e69418f65f678
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-06 21:28:51 -0600

    controller methods for new comedians and comedian id

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -6,12 +6,24 @@ class LaughTracksApp < Sinatra::Base
     specials  = comedians.find_specials
     erb :"comedians/index",
         locals: { comedians: comedians,
-                  specials: specials }
+                  specials:  specials   }
   end
 
-  get '/comedians.new' do
+  get '/comedians/new' do
     erb :"comedians/new"
+  end
 
+  post '/comedians' do
+    comedian = Comedian.create(params[:comedian])
+    redirect 'comedians/#{comedian.id}'
+  end
+
+  get 'comedians/:id' do
+    comedian = Comedian.find(params[:id])
+    specials.comedian.find_specials
+    erb :"comedians/comedian",
+        locals: { comedian: comedian,
+                  specials: specials  }
   end
 
 
